69aed6b79a64e0566d679500e3731e3304851584
hazim0001 <hazim_alsayed@hotmail.com>
2021-01-29 23:07:21 +0200

    job associations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,10 @@
 class Job < ApplicationRecord
   belongs_to :user
   belongs_to :customer
+
+  has_many :line_parts
+  has_many :part_inventories, through: :line_parts
+  has_many :line_labours
+  has_many :labour_jobs, through: :line_labours
+  has_many :mechanics, through: :line_labours
 end
