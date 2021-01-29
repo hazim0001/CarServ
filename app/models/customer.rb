76a7075aef165fc12a836c11215aa6f79e046840
hazim0001 <hazim_alsayed@hotmail.com>
2021-01-29 23:03:07 +0200

    customer associations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,9 @@
 class Customer < ApplicationRecord
   has_many :jobs
+  has_many :line_parts, through: :jobs
+  has_many :line_labours, through: :jobs
+  has_many :users, through: :jobs
+
 
   validates :name, presence: true
   validates :phone_number, presence: true, numericality: { only_integer: true }, length: { maximum: 11 }
