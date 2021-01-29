3ea97a59a538c4abb8edb8749860ea52c1175733
hazim0001 <hazim_alsayed@hotmail.com>
2021-01-29 16:55:55 +0200

    Ngrok setting in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.hosts << /[a-z0-9]+\.ngrok\.io/ # ngrok
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
