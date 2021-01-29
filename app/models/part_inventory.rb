f6024b3bbe271ee2ae7afefb6918b161a4e65511
hazim0001 <hazim_alsayed@hotmail.com>
2021-01-29 22:53:09 +0200

    part inventory associations

--- app/models/part_inventory.rb
+++ app/models/part_inventory.rb
@@ -1,2 +1,10 @@
 class PartInventory < ApplicationRecord
+  has_many :inventory_suppliers
+  has_many :part_suppliers, through: :inventory_suppliers
+
+  has_many :line_parts
+  has_many :jobs, through: :line_parts
+
+  has_many :inventory_restocks
+
 end
