5c7b3305b1fb807e9217e8221987db1a0ef8d825
hazim0001 <hazim_alsayed@hotmail.com>
2021-01-30 10:40:18 +0200

    routes v1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,23 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :jobs do
+    resources :line_parts, only: :create
+    resources :line_labour, only: :create
+  end
+
+  resources :part_suppliers
+  resources :labour_jobs
+  resources :mechanics
+  resources :customers
+  resources :part_inventories do
+    resources :inventory_suppliers, only: :create
+    resources :inventory_restocks, only: :create
+
+  end
+
+  resources :line_parts, only: %i[destroy update]
+  resources :line_labour, only: %i[destroy update]
+  resources :inventory_suppliers, only: %i[destroy update]
+  resources :inventory_restocks, only: %i[destroy]
 end
